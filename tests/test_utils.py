2b32a22be2653553739802aece23ec261fb37c6e
Alex Puffer <alex.puffer@gmail.com>
2020-01-17 21:19:59 -0800

    changed flatten_kwargs() to be private, as public use can easily break it.\n No handling is present for dicts of incorrect format, and no intention of adding handling.  The calling of this function will be done strictly internally.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,6 @@
-from winwifi.utils import flatten_kwargs
+from winwifi.utils import _flatten_kwargs
 
 def test_flatten_kwargs():
+    """Flatten simple dict into space separated string"""
     kwargs = {'key1': 'val1', 'key2': 'val2'}
-    assert flatten_kwargs(kwargs) == 'key1 val1 key2 val2'
+    assert _flatten_kwargs(kwargs) == 'key1 val1 key2 val2'
