6c252136b718446a1ec622873d944f6d913821c2
arpuffer <alex.puffer@gmail.com>
2020-01-20 21:51:33 -0800

    Update test_sample.py

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -2,10 +2,11 @@ import os
 
 
 def my_function(src_dir):
+    """This is the function we're testing.  executes 'ls some_dir/' in bash."""
     os.system('ls ' + src_dir)
     
-@unittest.mock.patch('os.system')
-def test_my_function(os_system):
+@unittest.mock.patch('os.system')  # This declares that we are mocking the os.system call made by my_function()
+def test_my_function(os_system):  # The input to the test function is the mocked os.system (you can name this anything, it's set up by the decorator above, but it's smart to name it sensibly)
     # type: (unittest.mock.Mock) -> None
     my_function("/path/to/dir")
-    os_system.assert_called_once_with('ls /path/to/dir')
+    os_system.assert_called_once_with('ls /path/to/dir')  # os_system is a python unittest MagicMock object that behaves like the function it mocks, but also has inherited methods from MagicMock, like the assert_called_with you see here
